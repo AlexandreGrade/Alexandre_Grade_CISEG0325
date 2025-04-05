97a438bbd179f2d6071761df4a956d55a5425ee3
AlexandreGrade <alex.pina.grade@gmail.com>
2025-04-05 01:26:50 +0100

    Delete exercicio1.py

--- exercicio1.py
+++
@@ -1,8 +0,0 @@
-segundos = int(input("Insira a quantidade de segundos: "))
-
-horas = segundos // 3600
-segundos = segundos % 3600
-minutos = segundos // 60
-segundos = segundos % 60
-
-print(f"{horas} horas, {minutos} minutos e {segundos} segundos")
