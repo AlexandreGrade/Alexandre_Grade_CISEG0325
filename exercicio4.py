73baed9165ac7b5833e09c8320dd3f13608464ac
AlexandreGrade <alex.pina.grade@gmail.com>
2025-04-05 01:27:09 +0100

    Delete exercicio4.py

--- exercicio4.py
+++
@@ -1,8 +0,0 @@
-saldo = float(input("Insira o saldo da conta: "))
-cheque = float(input("Insira o valor do cheque: "))
-
-if cheque <= saldo:
-    saldo -= cheque
-    print(f"Valor do cheque válido. Saldo atual: {saldo}")
-else:
-    print("Saldo insuficiente. Valor do cheque inválido.")
